bae6455217e66abcfdff5bb0e09c4193aa8438ba
DMSANCHEZQ <davidsqr6@gmail.com>
2023-10-08 14:06:17 -0500

    config: add web client configuration

---
+++ src/main/java/com/dasc/webclient/config/WebClientProperties.java
@@ -0,0 +1,7 @@
+package com.dasc.webclient.config;
+
+public class WebClientProperties {
+
+  public static final String BASE_URL = "http://api.springframework.guru";
+  public static final String BEER_v1_PATH = "/api/v1/beer";
+}
